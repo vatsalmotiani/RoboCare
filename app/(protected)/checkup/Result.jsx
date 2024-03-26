dcdd4f9283200935fdd2b14dd3646908c701333e
Vatsal Motiani <54301562+vatsalmotiani@users.noreply.github.com>
2024-03-26 17:03:50 +0530

    Result -> Accordian UI

--- app/(protected)/checkup/Result.jsx
+++ app/(protected)/checkup/Result.jsx
@@ -1,18 +1,65 @@
 "use client";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../../components/ui/card";
 import { Skeleton } from "../../../components/ui/skeleton";
-import { Check, ShieldAlert, ThumbsDown, ThumbsUp, X } from "lucide-react";
+import { Bot, Check, ShieldAlert, ThumbsDown, ThumbsUp, X } from "lucide-react";
 import { Button } from "../../../components/ui/button";
 import { useState } from "react";
+import Footer from "../../../components/Footer";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../../../components/ui/accordian";
 
 export default function Result({ responseData, loading, errorStatus }) {
   const [feedbackReturned, setFeedbackReturned] = useState(false);
 
-  const handleYes = () => {
+  const Feedback = () => {
+    return (
+      <div className='space-y-4'>
+        {!feedbackReturned && (
+          <>
+            <p className='text-lg text-neutral-500'>Did this information meet your satisfaction? </p>
+            <div className='flex space-x-2 '>
+              <Button
+                onClick={handleFeedback}
+                className='space-x-2 flex items-center text-neutral-500 bg-white hover:bg-neutral-200 border-neutral-300 border-1'
+              >
+                <ThumbsUp className='text-neutral-400' />
+                {/* <p>Yes</p> */}
+              </Button>
+              <Button
+                onClick={() => setFeedbackReturned(true)}
+                className='space-x-2 flex items-center text-neutral-500 bg-white hover:bg-neutral-200 border-neutral-300 border-1'
+              >
+                <ThumbsDown className='text-neutral-400' />
+                {/* <p>No</p> */}
+              </Button>
+            </div>
+          </>
+        )}
+        {feedbackReturned && (
+          <>
+            <div className='text-lg text-neutral-500 flex space-x-2'>
+              <Bot /> <p>Thank you for your response. This helps us refine our services.</p>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  };
+
+  const handleFeedback = () => {
     setFeedbackReturned(true);
     // Call API to send feedback
-    console.log("yes", responseData);
+    console.log("Yes", responseData);
   };
+
+  // Check if responseData is not null and has the percentages property
+  const sortedData =
+    responseData && responseData.percentages
+      ? // Convert responseData.percentages to an array of objects
+        Object.entries(responseData.percentages)
+          .map(([disease, percentage]) => ({ disease, percentage }))
+          // Sort the array of objects by percentage in descending order
+          .sort((a, b) => b.percentage - a.percentage)
+      : []; // Default to an empty array if responseData is null or doesn't have the percentages property
 
   return (
     <>
@@ -33,45 +80,64 @@ export default function Result({ responseData, loading, errorStatus }) {
         )}
         {!loading && responseData && (
           <div className='flex flex-col space-y-8'>
-            <div className='flex gap-x-4 flex-wrap'>
-              {Object.entries(responseData.percentages).map(([disease, percentage], i) => (
-                <Card
-                  className='mb-4 rounded-3xl p-6 text-white bg-blue-600 border-none flex flex-col items-center '
+            {/* <div className='flex gap-x-4 flex-wrap'> */}
+            <div className=''>
+              {sortedData.map(({ disease, percentage }, i) => (
+                <Accordion
                   key={i}
+                  type='single'
+                  collapsible
                 >
-                  <CardHeader className='text-xl'>{disease}</CardHeader>
-                  <CardContent className='text-6xl'>{percentage.toFixed(0)}%</CardContent>
-                </Card>
+                  <AccordionItem value={`item-${i}`}>
+                    <AccordionTrigger className=''>
+                      <div className='font-bold flex space-x-4 items-center'>
+                        <p className='font-medium  bg-blue-600 text-white py-2 px-4 rounded-full'>{i + 1}</p>
+                        <p className='text-2xl'>{disease}</p>
+                        <p className='text-blue-600'>{percentage.toFixed(0)}%</p>
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <div className='text-lg flex flex-col space-y-6 '>
+                        <p className=''>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                        <div className='space-y-1'>
+                          <p className='font-semibold text-blue-600'>Cause</p>
+                          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                        </div>
+                        <div className='space-y-1'>
+                          <p className='font-semibold text-blue-600'>Treatment</p>
+                          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                        </div>
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+
+                // --------------------------------------------
+
+                // <div key={i}>
+                //   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                //   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit veritatis aut enim earum, voluptates quia reiciendis exercitationem culpa incidunt.</p>
+                //   <p className='text-lg font-medium'>Cure</p>
+                //   <ol>
+                //     <li>Lorem ipsum dolor sit amet consectetur.</li>
+                //     <li>Lorem ipsum dolor sit amet consectetur.</li>
+                //     <li>Lorem ipsum dolor sit amet consectetur.</li>
+                //   </ol>
+                // </div>
+                // --------------------------------------------
+                // <Card
+                //   className='mb-4 rounded-3xl p-6 text-white bg-blue-600 border-none flex flex-col items-center '
+                //   key={i}
+                // >
+                //   <CardHeader className='text-xl'>{disease}</CardHeader>
+                //   <CardContent className='text-6xl'>{percentage.toFixed(0)}%</CardContent>
+                // </Card>
               ))}
             </div>
-            <div className='space-y-4'>
-              {!feedbackReturned && (
-                <>
-                  <p className='text-lg text-neutral-500'>Did this information meet your satisfaction? </p>
-                  <div className='flex space-x-2 '>
-                    <Button
-                      onClick={handleYes}
-                      className='space-x-2 flex items-center text-neutral-500 bg-white hover:bg-neutral-200 border-neutral-300 border-1'
-                    >
-                      <ThumbsUp className='text-neutral-400' />
-                      {/* <p>Yes</p> */}
-                    </Button>
-                    <Button
-                      onClick={() => setFeedbackReturned(true)}
-                      className='space-x-2 flex items-center text-neutral-500 bg-white hover:bg-neutral-200 border-neutral-300 border-1'
-                    >
-                      <ThumbsDown className='text-neutral-400' />
-                      {/* <p>No</p> */}
-                    </Button>
-                  </div>
-                </>
-              )}
-              {feedbackReturned && (
-                <>
-                  <p className='text-lg text-neutral-500'>Thank you for your response. This helps us refine our services.</p>
-                </>
-              )}
-            </div>
+            <Feedback />
+            <Footer />
           </div>
         )}
       </div>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,10 @@
 import { Info } from "lucide-react";
+import { Separator } from "../components/ui/separator";
 
 export default function Footer() {
   return (
     <div>
+      <Separator />
       <p className='mt-8 flex text-neutral-400 gap-x-2 bottom-0'>
         <Info /> The information provided here is for informational purposes only and should not be construed as medical advice. Please consult a doctor for any medical concerns. For immediate medical assistance in India, call the national helpline number: 104.
       </p>
